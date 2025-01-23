035f6d8b2c34957ddbabfb6d66b2b1d3bd4209d4
thenameisel <thenameiselwisman@gmail.com>
2025-01-23 09:46:33 -0400

    Set up Models

---
+++ Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace ShinyRockForum.Models
+{
+    public class Comment
+    {
+        //int CommentId
+        public int CommentId { get; set; }
+
+        //string Content
+        public string Content { get; set; } = string.Empty;
+        //datatime CreateDate
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        //int DiscussionId(foreign key)
+        public int DiscussionId { get; set; }
+    }
+}
---
+++ Models/Discussion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShinyRockForum.Models
+{
+    public class Discussion
+    {
+        //Discussion Id
+        public int DiscussionId { get; set; }
+
+        //Title
+        [Required(ErrorMessage = "A description is required.")]
+        public string Title { get; set; } = string.Empty;
+        //Content
+        [Required(ErrorMessage = "A description is required.")]
+        public string Content { get; set; } = string.Empty;
+
+        //ImageFilename
+        public string ImageFilename { get; set; } = string.Empty;
+
+        //CreateDate
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+    }
+}
